8b565117ceed452d25ddba46b1806da15707c881
James Bowlin <bitjam@gmail.com>
2019-05-29 11:21:42 -0600

    Bump version to 8.20.03  (mostly tsplash stuff)

--- live/menus/persist.menu.sv
+++ live/menus/persist.menu.sv
@@ -1,12 +1,12 @@
    1)  Standard
-   2)  persist_all     Save root in RAM, save home on disk (save root at shutdown)
-   3)  persist_root    Save root and home in RAM then saved at shutdown
-   4)  persist_static  Save root and home on disk with home separate on disk
-   5)  p_static_root   Save root and home on disk together
+   2)  persist_all     Spara root i RAM, spara home på disken (spara root vid avstängning)
+   3)  persist_root    Spara root och home i RAM sedan sparade vid avstängning
+   4)  persist_static  Spara root och home på disken med home separat på disken
+   5)  p_static_root   Spara root och home på disken tillsammans
    6)  persist_home    Bara home-persistens
-   7)  frugal_persist  Frugal with root in RAM and home on disk
-   8)  frugal_root     Frugal with root and home in RAM then saved at shutdown
-   9)  frugal_static   Frugal with root on disk and home separate on disk
-  10)  f_static_root   Frugal with root and home on disk together
+   7)  frugal_persist  Frugal med root i RAM och home på disken
+   8)  frugal_root     Frugal med root och home i RAM sedan sparade vid avstängning
+   9)  frugal_static   Frugal med root på disken och home separat på disken
+  10)  f_static_root   Frugal med root och home på disken tillsammans
   11)  frugal_home     Frugal med endast home-persistens
   12)  frugal_only     Bara Frugal, ingen persistens
